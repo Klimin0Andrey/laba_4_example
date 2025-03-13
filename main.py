68bb2fb16b26cee15792f53b86e34936aaf16736
Klimin0Andrey <148792157+Klimin0Andrey@users.noreply.github.com>
2025-03-13 19:35:01 +0300

    Добавление функций в main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from math import sqrt
+
+
 class Calculator:
     def __init__(self, x, y):
         self.x = x
@@ -15,13 +18,32 @@ class Calculator:
     def divide(self):
         return self.x / self.y
 
+    def square_root(self):
+        value = input("Корень какого числа, вы хотите вычислить a или b? ")
+        if value == 'a':
+            return sqrt(self.x)
+        else:
+            return sqrt(self.y)
+
+    def exponentiation(self):
+        return self.x ** self.y
+
 
 if __name__ == "__main__":
     a, b = map(int, input("Введите значения переменных a, b: ").split())
 
     solution = Calculator(a, b)
     while True:
-        your_choice = int(input("Введите команду Калькулятора: \n1)Сумма\n2)Минус\n3)Произведение\n4)Деление\n5)Выход\n"))
+        your_choice = int(input(
+            "Введите команду Калькулятора: \n"
+            "1)Сумма\n"
+            "2)Минус\n"
+            "3)Произведение\n"
+            "4)Деление\n"
+            "5)Квадратный корень\n"
+            "6)Возведение в степень\n"
+            "7)Выход\n"))
+
         match your_choice:
             case 1:
                 print(solution.summary())
@@ -32,4 +54,8 @@ if __name__ == "__main__":
             case 4:
                 print(solution.divide())
             case 5:
+                print(solution.square_root())
+            case 6:
+                print(solution.exponentiation())
+            case 7:
                 break
